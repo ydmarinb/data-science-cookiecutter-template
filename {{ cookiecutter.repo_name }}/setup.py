e8aa490c1eac50cd6eb625d60ff93707db6f9033
Yubar Daniel Marín Benjumea <danielmarinbe@gmail.com>
2022-07-08 11:02:52 -0500

    arreglo lincecia en setup

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -5,6 +5,5 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='{{ cookiecutter.description }}',
-    author='{{ cookiecutter.author_name }}',
-    license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}',
+    author='{{ cookiecutter.author_name }}'
 )
